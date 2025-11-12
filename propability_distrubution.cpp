02a69eb8e85d37b190d3f398c9b16ff5e92a51ca
Vladislav Foss <vladislf@uio.no>
2025-11-12 15:50:43 +0100

    modifying montecarlo class to include the histogram needed for assignment 6. Figs from burn-in added into a folder as well

---
+++ propability_distrubution.cpp
@@ -0,0 +1,15 @@
+#include "MonteCarlo.hpp"
+#include "IsingModel.hpp"
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <filesystem>
+
+
+int main(){
+
+    int L = 20;
+    double T1 = 1.0;
+    double T2 = 2.4;
+
+}
